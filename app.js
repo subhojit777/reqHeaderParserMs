a5aff0d6f1f6deb893c4ec81d1233476353b753b
Subhojit Paul <subhojitpaul21@gmail.com>
2016-07-31 18:35:04 +0530

    trial show the requester's ip

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var publicIp = require('public-ip');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -14,15 +15,23 @@ app.get('/api/whoami', function(req, res) {
     'path': '/ip'
   };
 
-  http.get(options, function(response) {
-    response.setEncoding('utf8');
+  //http.get(options, function(response) {
+    //response.setEncoding('utf8');
 
-    response.on('data', function(chunk) {
-      res.status(200).json({
-        'ipaddress': chunk.trim(),
-        'language': req.headers['accept-language'],
-        'software': req.headers['user-agent']
-      });
+    //response.on('data', function(chunk) {
+      //res.status(200).json({
+        //'ipaddress': chunk.trim(),
+        //'language': req.headers['accept-language'],
+        //'software': req.headers['user-agent']
+      //});
+    //});
+  //});
+
+  publicIp.v4().then(ip => {
+    res.status(200).json({
+      'ipaddress': ip,
+      'language': req.headers['accept-language'],
+      'software': req.headers['user-agent']
     });
   });
 });
